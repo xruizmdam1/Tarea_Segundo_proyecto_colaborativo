c83edaafda7346b46b77fdec6040adbf465d04bd
paquerretaadam1 <paquerretaadam1@gmail.com>
2021-09-28 11:02:15 +0200

    quito el método main pesados de mierda <3

--- 1.7.2. Tarea - Segundo proyecto colaborativo/PedroJ.cs
+++ 1.7.2. Tarea - Segundo proyecto colaborativo/PedroJ.cs
@@ -6,10 +6,6 @@ namespace _1._7._2._Tarea___Segundo_proyecto_colaborativo
 {
     class PedroJ
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Soy Pedro");
-        }
         public static void hoola()
         {
             Console.WriteLine("Soy Pedro");
