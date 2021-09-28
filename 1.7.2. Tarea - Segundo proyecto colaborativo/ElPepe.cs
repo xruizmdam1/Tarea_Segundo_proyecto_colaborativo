e348d0349e3315f076db9872c4cac4279b82fff8
xruizmdam1 <xruizmdam1@gmail.com>
2021-09-28 10:54:09 +0200

    Añadida clase ElPepe

---
+++ 1.7.2. Tarea - Segundo proyecto colaborativo/ElPepe.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _1._7._2._Tarea___Segundo_proyecto_colaborativo
+{
+    class ElPepe
+    {
+        void Saludar()
+        {
+            Console.WriteLine("¡Hola desde la clase ElPepe");
+        }
+    }
+}
