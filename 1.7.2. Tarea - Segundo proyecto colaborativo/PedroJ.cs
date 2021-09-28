5587b3b37871a6ea9bb20a21ad2e7289bbcb05ae
paquerretaadam1 <paquerretaadam1@gmail.com>
2021-09-28 10:52:05 +0200

    Añado mi clase

---
+++ 1.7.2. Tarea - Segundo proyecto colaborativo/PedroJ.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _1._7._2._Tarea___Segundo_proyecto_colaborativo
+{
+    class PedroJ
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Soy Pedro");
+        }
+    }
+}
